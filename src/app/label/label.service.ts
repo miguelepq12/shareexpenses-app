410b67cf47f36d885c7cc0f90c4aa95026bf5e40
miguelepq12 <miguelepq@gmail.com>
2019-12-02 13:38:15 -0400

    0.0.8: Creado servicio de etiquetas

--- src/app/label/label.service.ts
+++ src/app/label/label.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import {VariablesApp} from '../helpers/variables-app';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {Label} from './label';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LabelService {
+  private URL_LABEL = VariablesApp.URL_API + 'labels/';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /*
+    Retorno Json: labels (Lista de label)
+   */
+  getLabels(page): Observable<Label[]> {
+    return this.http.get<any>(this.URL_LABEL + '?page=' + page).pipe(
+      map(response => {
+        return response.labels as Label[];
+      }),
+      catchError(err => {
+        return throwError(err);
+      })
+    );
+  }
+
+  /*
+    Retorna Json:
+   */
+  getLabel(idLabel): Observable<Label> {
+    return this.http.get<Label>(this.URL_LABEL + Label).pipe(
+      map(response => {
+        return response;
+      }),
+      catchError(err => {
+        return throwError(err);
+      })
+    );
+  }
+
+  createLabel(label: Label): Observable<Label> {
+    return this.http.post<Label>(this.URL_LABEL, label, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        return throwError(e);
+      })
+    );
+  }
+
+  updateLabel(label: Label): Observable<Label> {
+    return this.http.put<Label>(this.URL_LABEL + label.id, label, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        return throwError(e);
+      })
+    );
+  }
+
+  deleteLabel(label: Label): Observable<any> {
+    return this.http.delete<any>(this.URL_LABEL + label.id).pipe(
+      catchError(e => {
+        return throwError(e);
+      })
+    );
+  }
 }
